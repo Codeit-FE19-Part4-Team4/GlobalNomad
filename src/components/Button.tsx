5d694ce61265878709f332e334e8eda1a8c7e699
sylee <linggo01@naver.com>
2025-12-20 12:10:29 +0900

    ✨ Feat: cva 사용한 스타일 정의

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { cva, type VariantProps } from 'class-variance-authority';
 import Link from 'next/link';
 import React, { ComponentPropsWithoutRef, ElementType } from 'react';
 import { cn } from '@/util/cn';
@@ -34,6 +35,33 @@ export type ButtonVariant = 'primary' | 'secondary' | 'tertiary';
 // 버튼 크기 구분
 export type ButtonSize = 'xl' | 'lg' | 'md' | 'sm' | 'xs';
 
+// 버튼 스타일 정의
+const ButtonVariants = cva(
+  'flex items-center justify-center gap-1 text-body-lg text-white font-[var(--weight-title-lg)] tracking-[var(--tracking-title-lg)] cursor-pointer disabled:cursor-default',
+  {
+    variants: {
+      size: {
+        xl: 'h-[54px] rounded-[16px]',
+        lg: 'h-[50px] rounded-[14px]',
+        md: 'h-[41px] rounded-[12px] md:h-[47px] md:rounded-[14px]',
+        sm: 'h-[41px] rounded-[12px] text-[14px]',
+        xs: 'h-[37px] rounded-[8px]  md:h-[29px] md:w-[71px] text-[14px] text-gray-600 font-[var(--weight-title-sm)] ',
+      },
+      variant: {
+        primary:
+          'bg-primary-500 text-white hover:bg-[#2A86D9] disabled:bg-gray-200',
+        secondary:
+          'bg-white text-gray-600 border border-gray-200 hover:bg-gray-25',
+        tertiary: 'bg-gray-50 text-gray-600 hover:bg-gray-100',
+      },
+    },
+    defaultVariants: {
+      size: 'lg',
+      variant: 'primary',
+    },
+  }
+);
+
 // 제네릭 T를 사용해 button, a, Link 등 다양한 요소를 지원
 export type ButtonProps<T extends ElementType> = {
   as?: T;
@@ -46,26 +74,8 @@ export type ButtonProps<T extends ElementType> = {
   className?: string;
   children: React.ReactNode;
   prefix?: React.ReactNode;
-} & Omit<ComponentPropsWithoutRef<T>, 'as' | 'variant' | 'prefix'>;
-
-// 공통 기본 스타일
-const defaultClasses: string =
-  'flex items-center justify-center gap-1 text-body-lg text-white font-[var(--weight-title-lg)] tracking-[var(--tracking-title-lg)] cursor-pointer disabled:cursor-default';
-
-// 색상/역할별 스타일
-const variantClasses: Record<ButtonVariant, string> = {
-  primary: 'bg-primary-500 text-white hover:bg-[#2A86D9] disabled:bg-gray-200',
-  secondary: 'bg-white text-gray-600 border border-gray-200 hover:bg-gray-25',
-  tertiary: 'bg-gray-50 text-gray-600 hover:bg-gray-100',
-};
-// 크기별 스타일 (xl:로그인, lg:예약하기, md:상단타이틀,컴펌모달, sm:페이지하단, xs:카드안)
-const sizeClasses: Record<ButtonSize, string> = {
-  xl: 'h-[54px] rounded-[16px]',
-  lg: 'h-[50px] rounded-[14px]',
-  md: 'h-[41px] rounded-[12px] md:h-[47px] md:rounded-[14px]',
-  sm: 'h-[41px] rounded-[12px] text-[14px]',
-  xs: 'h-[37px] rounded-[8px]  md:h-[29px] md:w-[71px] text-[14px] text-gray-600 font-[var(--weight-title-sm)] ',
-};
+} & Omit<ComponentPropsWithoutRef<T>, 'as' | 'variant' | 'prefix'> &
+  VariantProps<typeof ButtonVariants>;
 
 export default function Button<T extends ElementType = 'button'>({
   as,
@@ -78,14 +88,11 @@ export default function Button<T extends ElementType = 'button'>({
   ...props
 }: ButtonProps<T>) {
   const Component = as || 'button';
-  const buttonClass = cn(
-    defaultClasses,
-    sizeClasses[size],
-    variantClasses[variant],
-    className
-  );
   return (
-    <Component className={buttonClass} onClick={onClick} {...props}>
+    <Component
+      className={cn(ButtonVariants({ size, variant }), className)}
+      onClick={onClick}
+      {...props}>
       {prefix}
       {children}
     </Component>
