183a34a5d1b009aa94c0e8df6cf8b1d63173e307
yujin-fe <hyujin1224@gmail.com>
2025-12-21 21:50:20 +0900

    ✨ Feat: Tab 컴포넌트 구현

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,18 +1,62 @@
-const mockdata = [
-  { id: 1, content: '신청', number: 2 },
-  { id: 2, content: '승인', number: 3 },
-  { id: 3, content: '거절', number: 0 },
-];
-export default function Tab() {
+'use client';
+import { useState } from 'react';
+
+import { type ReservedSchedule } from '@/types/reserved-schedule';
+import { cn } from '@/util/cn';
+
+interface TabPropsType {
+  data: ReservedSchedule;
+  onClick?: (status: '신청' | '승인' | '거절') => void;
+}
+interface TabDataType {
+  id: number;
+  status: '신청' | '승인' | '거절';
+  count: number;
+}
+/**
+ * props의 data는
+ * GET /my-activities/{activityId}/reserved-schedule
+ * 응답을 그대로 사용합니다.
+ */
+export default function Tab({ data, onClick }: TabPropsType) {
+  const tabData: TabDataType[] = [
+    { id: 1, status: '신청', count: data.count.pending },
+    { id: 2, status: '승인', count: data.count.confirmed },
+    { id: 3, status: '거절', count: data.count.declined },
+  ];
+  const [activeTab, setActiveTab] = useState(tabData[0].id);
+
   return (
-    <div className="flex justify-start gap-2 border-b border-gray-100">
-      {mockdata.map((data) => (
-        <div
-          key={data.id}
-          className="flex flex-1 justify-center gap-1 px-3.5 py-[11px]">
-          <span>{data.content}</span>
-          <span>{data.number}</span>
-        </div>
+    <div className="flex justify-start">
+      {tabData.map((tab) => (
+        <button
+          type="button"
+          key={tab.id}
+          className={cn(
+            'flex flex-1 justify-center gap-1 border-b px-3.5 py-[11px]',
+            tab.id === activeTab
+              ? 'border-primary-500 text-primary-500'
+              : 'border-gray-100'
+          )}
+          onClick={() => {
+            onClick?.(tab.status);
+            setActiveTab(tab.id);
+          }}>
+          <span
+            className={cn(
+              'text-[16px]',
+              activeTab === tab.id ? 'font-bold' : 'font-normal'
+            )}>
+            {tab.status}
+          </span>
+          <span
+            className={cn(
+              'text-[16px]',
+              activeTab === tab.id ? 'font-bold' : 'font-normal'
+            )}>
+            {tab.count}
+          </span>
+        </button>
       ))}
     </div>
   );
