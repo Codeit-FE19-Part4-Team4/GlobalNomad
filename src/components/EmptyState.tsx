98683dfb113df80562512b0afa278d8afc7e132b
양은지 <eunji124@github.com>
2026-01-02 15:32:53 +0900

    ✨ Feat: 예약 체험 비었을때

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import emptyImage from '@/assets/images/common/img-empty.svg';
+
+type EmptyStateProps = {
+  description?: string;
+  buttonText: string;
+  buttonHref: string;
+};
+
+export default function EmptyState({
+  description,
+  buttonText,
+  buttonHref,
+}: EmptyStateProps) {
+  return (
+    <div className="flex flex-col items-center justify-center py-20 text-center">
+      {/* 이미지 */}
+      <Image src={emptyImage} alt="" width={122} height={122} />
+
+      {/* 텍스트 */}
+      {description && (
+        <p className="mt-6 text-[17px] font-medium text-gray-600">
+          {description}
+        </p>
+      )}
+
+      {/* 버튼 */}
+      <Link
+        href={buttonHref}
+        className="bg-primary-500 mt-6 inline-block rounded-md px-6 py-2 text-white">
+        {buttonText}
+      </Link>
+    </div>
+  );
+}
