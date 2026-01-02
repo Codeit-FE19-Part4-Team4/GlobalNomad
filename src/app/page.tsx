08bb10c6f405ab44a0e66b3f211934afb1c0e5c9
eunji0124 <76269203+eunji0124@users.noreply.github.com>
2026-01-03 08:50:34 +0900

    [✨ Feature] 사이드 메뉴 모바일 디자인 변경 (#92)
    Co-authored-by: 양은지 <eunji124@github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,20 @@
-'use client';
+import EmptyState from '@/components/EmptyState';
+import Text from '@/components/Text';
 
-import { useState } from 'react';
-
-import ScheduleForm from '@/components/ScheduleForm';
-import { ScheduleBase } from '@/components/ScheduleForm/schedule-type';
-const data = {
-  schedules: [
-    // {
-    //   id: 1,
-    //   date: '2023-12-01',
-    //   startTime: '12:00',
-    //   endTime: '13:00',
-    // },
-    // {
-    //   id: 2,
-    //   date: '2023-12-05',
-    //   startTime: '12:00',
-    //   endTime: '13:00',
-    // },
-  ],
-};
 export default function TestPage() {
-  const [schedulesToAdd, setSchedulesToAdd] = useState<ScheduleBase[]>([]);
-  const [scheduleIdsToRemove, setScheduleIdsToRemove] = useState<number[]>([]);
   return (
-    <ScheduleForm
-      initialSchedules={data.schedules}
-      setSchedulesToAdd={setSchedulesToAdd}
-      setScheduleIdsToRemove={setScheduleIdsToRemove}
-    />
+    <>
+      <div className="space-y-4 p-10">
+        <Text as="h2" className="mb-6 text-2xl font-bold">
+          Global Nomad
+        </Text>
+
+        <EmptyState
+          description="아직 등록한 체험이 없어요."
+          buttonText="체험 등록하기"
+          buttonHref="/experiences"
+        />
+      </div>
+    </>
   );
 }
