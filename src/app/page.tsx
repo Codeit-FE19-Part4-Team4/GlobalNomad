29c9ccb5082ea6f83dfce4f61d4ee1bf009e1c5a
sylee <linggo01@naver.com>
2026-01-03 02:32:47 +0900

    ✨ Feat: 예약 가능 시간 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
-import Text from '@/components/Text';
+'use client';
+
+import { useState } from 'react';
+
+import ScheduleForm from '@/components/ScheduleForm';
+import { ScheduleBase } from '@/components/ScheduleForm/schedule-type';
+const data = {
+  schedules: [
+    // {
+    //   id: 1,
+    //   date: '2023-12-01',
+    //   startTime: '12:00',
+    //   endTime: '13:00',
+    // },
+    // {
+    //   id: 2,
+    //   date: '2023-12-05',
+    //   startTime: '12:00',
+    //   endTime: '13:00',
+    // },
+  ],
+};
 export default function TestPage() {
+  const [schedulesToAdd, setSchedulesToAdd] = useState<ScheduleBase[]>([]);
+  const [scheduleIdsToRemove, setScheduleIdsToRemove] = useState<number[]>([]);
   return (
-    <div className="space-y-4 p-10">
-      <Text as="h2" className="mb-6 text-2xl font-bold">
-        Global Nomad
-      </Text>
-    </div>
+    <ScheduleForm
+      initialSchedules={data.schedules}
+      setSchedulesToAdd={setSchedulesToAdd}
+      setScheduleIdsToRemove={setScheduleIdsToRemove}
+    />
   );
 }
